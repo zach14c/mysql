8832e4147c7916598027171b78299b66bebd19d9
Christopher Powers <cpowers@mysql.com>
2008-08-14 00:45:00 -0500

    Disabled new asserts in Transaction::releaseDependecies introduced in commit: http://lists.mysql.com/commits/51491

--- storage/falcon/Transaction.cpp
+++ storage/falcon/Transaction.cpp
@@ -783,8 +783,9 @@ void Transaction::releaseDependencies()
 
 			if (COMPARE_EXCHANGE_POINTER(&state->transaction, transaction, NULL))
 				{
-				ASSERT(transaction->transactionId == state->transactionId || transaction->transactionId == 0);
-				ASSERT(transaction->state == Active || transaction->state == Available);
+				//ASSERT(transaction->transactionId == state->transactionId || transaction->transactionId == 0);
+				ASSERT(transaction->transactionId == state->transactionId || transaction->state == Available);
+				//ASSERT(transaction->state == Active || transaction->state == Available);
 				transaction->releaseDependency();
 				}
 			}
