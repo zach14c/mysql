4fc3be499c735727951ec8024971facad13b01bd
Tomas Ulin <tomas.ulin@sun.com>
2008-09-01 17:25:21 +0200

    correct mysql-test-run w.r.t extra suites

--- mysql-test/mysql-test-run.pl
+++ mysql-test/mysql-test-run.pl
@@ -404,17 +404,18 @@ sub main () {
       # Check for any extra suites to enable based on the path name
       my %extra_suites=
 	(
-	 "mysql-5.1-new-ndb"              => "ndb_team",
-	 "mysql-5.1-new-ndb-merge"        => "ndb_team",
-	 "mysql-5.1-telco-6.2"            => "ndb_team",
-	 "mysql-5.1-telco-6.2-merge"      => "ndb_team",
-	 "mysql-5.1-telco-6.3"            => "ndb_team",
-	 "mysql-6.0-ndb"                  => "ndb_team",
-	 "mysql-6.0-falcon"               => "falcon_team",
-	 "mysql-6.0-falcon-team"          => "falcon_team",
-	 "mysql-6.0-falcon-wlad"          => "falcon_team",
-	 "mysql-6.0-falcon-chris"         => "falcon_team",
-	 "mysql-6.0-falcon-kevin"         => "falcon_team",
+	 "bzr_mysql-5.1-ndb"                  => "ndb_team",
+	 "bzr_mysql-5.1-ndb-merge"            => "ndb_team",
+	 "bzr_mysql-5.1-telco-6.2"            => "ndb_team",
+	 "bzr_mysql-5.1-telco-6.2-merge"      => "ndb_team",
+	 "bzr_mysql-5.1-telco-6.3"            => "ndb_team",
+	 "bzr_mysql-5.1-telco-6.4"            => "ndb_team",
+	 "bzr_mysql-6.0-ndb"                  => "ndb_team,rpl_ndb_big",
+	 "bzr_mysql-6.0-falcon"               => "falcon_team",
+	 "bzr_mysql-6.0-falcon-team"          => "falcon_team",
+	 "bzr_mysql-6.0-falcon-wlad"          => "falcon_team",
+	 "bzr_mysql-6.0-falcon-chris"         => "falcon_team",
+	 "bzr_mysql-6.0-falcon-kevin"         => "falcon_team",
 	);
 
       foreach my $dir ( reverse splitdir($glob_basedir) )
