546ee92a936c804c55da0845895c37b172920ae4
Rafal Somla <Rafal.Somla@sun.com>
2008-09-11 11:47:53 +0200

    BUG#37522 (Backup: stream_v1_transport: Error condition should be checked for as_* calls)
    This patch adds checks for errors in stream_v1_transport.c where the low level I/O 
    functions are called.

--- sql/backup/stream.cc
+++ sql/backup/stream.cc
@@ -585,6 +585,10 @@ void Output_stream::close()
   if (m_fd < 0)
     return;
 
+  /*
+   Note: Even if bstream_close() fails we want to do the lower level clean-up.
+   This is why errors from bstream_close() are ignored.
+  */ 
   bstream_close(this);
 #ifdef HAVE_COMPRESS
   if (m_with_compression)
@@ -626,7 +630,8 @@ void Output_stream::close()
 */
 bool Output_stream::rewind()
 {
-  bstream_close(this);
+  if (bstream_close(this) != BSTREAM_OK)
+    return FALSE;
 
   bool ret= Stream::rewind();
 
@@ -771,6 +776,10 @@ void Input_stream::close()
   if (m_fd < 0)
     return;
 
+  /*
+   Note: Even if bstream_close() fails we want to do the lower level clean-up.
+   This is why errors from bstream_close() are ignored.
+  */ 
   bstream_close(this);
 #ifdef HAVE_COMPRESS
   if (m_with_compression)
@@ -792,7 +801,8 @@ void Input_stream::close()
 */
 bool Input_stream::rewind()
 {
-  bstream_close(this);
+  if (bstream_close(this) != BSTREAM_OK)
+    return FALSE;
 
   bool ret= Stream::rewind();
 
