8a925050364e0fdb51ffe47a6d2a023c2c2a0373
Hakan Kuecuekyilmaz <hky@sun.com>
2008-10-21 16:44:05 +0200

    Bumped internal version number.

--- storage/falcon/StorageVersion.h
+++ storage/falcon/StorageVersion.h
@@ -14,5 +14,5 @@
    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
 
 
-#define FALCON_VERSION	"T1.2-7"
-#define FALCON_DATE		"16 October, 2008"
+#define FALCON_VERSION	"T1.2-8"
+#define FALCON_DATE		"21 October, 2008"
