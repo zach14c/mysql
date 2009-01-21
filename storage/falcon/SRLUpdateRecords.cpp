b2429cf6a184a6daa8d2c16679922d1ba40f34c5
Christopher Powers <christopher.powers@sun.com>
2009-01-20 18:35:50 -0600

    Bug #42131 falcon_backlog test crashes in Record::getEncodedValue
    Record chill now exclusively locks the record version chain before deleting
    the record memory.

--- storage/falcon/SRLUpdateRecords.cpp
+++ storage/falcon/SRLUpdateRecords.cpp
@@ -42,6 +42,13 @@ bool SRLUpdateRecords::chill(Transaction *transaction, RecordVersion *record, ui
 {
 	Sync syncPrior(record->getSyncPrior(), "SRLUpdateRecords::chill");
 	
+	// Exclusively lock the record chain before chilling the record. Use a 50ms wait to defer
+	// to other tasks accessing the record chain.
+	//
+	// This is a provisional resolution to a syncPrior/syncWrite deadlock that can be triggered
+	// by a low-level thaw on a concurrent thread already holding syncPrior. Such a deadlock
+	// can occur while pruning record versions during during a scavenge.
+	
 	try
 		{
 		syncPrior.lock(Exclusive, 50);
