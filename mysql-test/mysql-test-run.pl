c65d60ce6375f9275103e6abfe0ca11a486e1817
Dmitry Lenev <dlenev@mysql.com>
2009-02-05 11:59:29 +0300

    Fix for bug #41688 "Foreign keys: lock warnings" and bug #42069 "Foreign keys: hang if two connections".
    For Falcon table concurrent execution of statement which in       
    general case should acquire TL_READ_NO_INSERT lock on the table
    (e.g. multi-update, DML with subqueries, or statements involving    
    new foreign key checks) and statement modifying this table might       
    have led to warnings in error log (bug #41688) and even to  
    deadlocks (when several tables and transactions were involved -
    bug #42069).
    
    Both these problems were caused either by combination of the
    following two factors:
    - First, Falcon didn't downgrade TL_READ_NO_INSERT locks even
      although it was downgrading TL_WRITE to TL_WRITE_ALLOW_WRITE
      for one of tables in the same statement. This created
      possibility of deadlock between Falcon row-level and server
      table-level locks (as in this case it was possible for some
      statement to be waiting on some row-level lock while holding
      table-level lock which prevented owner of the former from
      proceeding). This also made statements using Falcon tables
      especially susceptible to the second factor.
    - Second, our table-level locking subsystem were erroneously
      allowing to acquire TL_READ_NO_INSERT lock on the table
      even if there was another thread having TL_WRITE_ALLOW_WRITE
      on it (while assuming that TL_READ_NO_INSERT is incompatible
      with TL_WRITE_ALLOW_WRITE). So in debug build this led to
      warning popping up in error log. It probably didn't cause
      more severe consequences since engines who downgrade write
      locks are likely to implement its own additional support for
      table locks in order to ensure that they also take into account
      all row-locks acquired by transactions (e.g. see InnoDB).
    
    This fix deals with the first issue by changing Falcon in such way
    that it downgrades locks of type TL_READ_NO_INSERT to TL_READ
    (unless LOCK TABLES is being executed). This is safe since Falcon
    as any row-locker/MVCC engine has its own mechanism for handling
    concurrent changes. It should also increase concurrency in certain
    scenarios.
    
    Fixing the second factor will require much more intrusive changes
    and will be done as part of work on bug 42147 "Concurrent DML and
    LOCK TABLE ... READ for InnoDB table cause warnings in errlog".

--- mysql-test/mysql-test-run.pl
+++ mysql-test/mysql-test-run.pl
@@ -3484,6 +3484,7 @@ sub extract_warning_lines ($) {
      # qr/^Warning:|mysqld: Warning|\[Warning\]/,
      # qr/^Error:|\[ERROR\]/,
      qr/^Warning:|mysqld: Warning/,
+     qr/^Warning at/,
      qr/^Error:/,
      qr/^==.* at 0x/,
      qr/InnoDB: Warning|InnoDB: Error/,
